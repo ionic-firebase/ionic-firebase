6f1d408c8cf067d126ae9ad1255a9f1adbd05ca9
ionic-firebase <162046126+ionic-firebase@users.noreply.github.com>
2024-03-04 20:24:57 +0000

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,10 +1,10 @@
 const firebaseCredentials = {
-    apiKey: 'AIzaSyDgAiE58ibCYOlULlj5VG0eO9NSAtPUPEI',
-    authDomain: 'myapp-53d92.firebaseapp.com',
-    databaseURL: 'https://myapp-53d92.firebaseio.com',
-    projectId: 'myapp-53d92',
-    storageBucket: 'myapp-53d92.appspot.com',
-    messagingSenderId: '585754205447',
-    appId: '1:585754205447:web:c06f5388a9e979b5f739a2'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
 }
 export default firebaseCredentials;
